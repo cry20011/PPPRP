1e105402fcf9891fe25fb7486c4a73b82ca40fa9
cry20011 <sergeynovik123@mail.ru>
2023-05-10 21:55:59 +0300

    manifests sh

--- task2_istio.sh
+++ task2_istio.sh
@@ -9,6 +9,9 @@ kubectl delete -f ingress_virtual_service.yaml
 kubectl delete -f egress_service_entry.yaml
 kubectl delete -f egress_gateway.yaml
 kubectl delete -f egress_virtual_service.yaml
+kubectl delete -f ingress_tls_gateway.yaml
+kubectl delete -f ingress_tls_virtual_service.yaml
+
 
 kubectl apply -f first_app.yaml
 kubectl apply -f second_app.yaml
@@ -17,3 +20,5 @@ kubectl apply -f ingress_virtual_service.yaml
 kubectl apply -f egress_service_entry.yaml
 kubectl apply -f egress_gateway.yaml
 kubectl apply -f egress_virtual_service.yaml
+kubectl apply -f ingress_tls_gateway.yaml
+kubectl apply -f ingress_tls_virtual_service.yaml
