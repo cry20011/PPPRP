1fe15297acc1bf9224f53adf47480f0d3b372320
cry20011 <sergeynovik123@mail.ru>
2023-05-10 22:52:28 +0300

    mutual tls

--- tls_curl.sh
+++ tls_curl.sh
@@ -1,1 +1,3 @@
-curl -v -HHost:app.example.com --resolve "app.example.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" --cacert certs/example.com.crt "https://app.example.com:$SECURE_INGRESS_PORT/api/data"
+curl -v -HHost:app.example.com --resolve "app.example.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+  --cacert certs/example.com.crt --cert certs/client.example.com.crt --key certs/client.example.com.key \
+  "https://app.example.com:$SECURE_INGRESS_PORT/api/data"
