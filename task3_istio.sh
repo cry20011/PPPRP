c0ccc1d988dfa65ddff124e0c327350b9d15acc6
cry20011 <sergeynovik123@mail.ru>
2023-05-11 21:25:06 +0300

    task3 sh

--- task3_istio.sh
+++ task3_istio.sh
@@ -6,9 +6,6 @@ kubectl delete -f first_app.yaml
 kubectl delete -f second_app.yaml
 kubectl delete -f ingress_gateway.yaml
 kubectl delete -f ingress_virtual_service.yaml
-kubectl delete -f egress_service_entry.yaml
-kubectl delete -f egress_gateway.yaml
-kubectl delete -f egress_virtual_service.yaml
 kubectl delete -f egress_mtls_gateway.yaml
 kubectl delete -f egress_mtls_virtual_service.yaml
 kubectl delete -f egress_mtls_destination_rule.yaml
@@ -17,9 +14,6 @@ kubectl apply -f first_app.yaml
 kubectl apply -f second_app.yaml
 kubectl apply -f ingress_gateway.yaml
 kubectl apply -f ingress_virtual_service.yaml
-kubectl apply -f egress_service_entry.yaml
-kubectl apply -f egress_gateway.yaml
-kubectl apply -f egress_virtual_service.yaml
 kubectl apply -f egress_mtls_gateway.yaml
 kubectl apply -f egress_mtls_virtual_service.yaml
 kubectl apply -f egress_mtls_destination_rule.yaml
